27f9867f32b00d8689a91b3a28ea3f8c39bb4f06
Adarsh Bajpai <adarshbajpai67@gmail.com>
2022-10-15 02:12:30 +0530

    added: bus flight and train

--- scrapping/test.js
+++ scrapping/test.js
@@ -1,5 +1,9 @@
 const puppeteer = require('puppeteer');
 
+function input(){
+    scrapePrice('https://hotel.yatra.com/hotel-search/dom/search?checkoutDate=17/10/2022&checkinDate=15/10/2022&roomRequests[0].id=1&roomRequests[0].noOfAdults=2&roomRequests[0].noOfChildren=0&source=BOOKING_ENGINE&pg=1&tenant=B2C&isPersnldSrp=1&city.name=Goa&city.code=Goa&state.name=GOA&state.code=GOA&country.name=India&country.code=IND','//*[@id="result0"]/div[1]/div[2]/ul/li[4]/div[2]/p/span');
+}
+
 async function scrapePrice(url,path){
     const browser = await puppeteer.launch({headless: false});
     const page = await browser.newPage();
@@ -16,4 +20,6 @@ async function scrapePrice(url,path){
 
 
 // const destination_date="DEL-BLR-15/10/2022"
-scrapePrice('https://hotel.yatra.com/hotel-search/dom/search?checkoutDate=17/10/2022&checkinDate=15/10/2022&roomRequests[0].id=1&roomRequests[0].noOfAdults=2&roomRequests[0].noOfChildren=0&source=BOOKING_ENGINE&pg=1&tenant=B2C&isPersnldSrp=1&city.name=Goa&city.code=Goa&state.name=GOA&state.code=GOA&country.name=India&country.code=IND','//*[@id="result0"]/div[1]/div[2]/ul/li[4]/div[2]/p/span')
+
+
+module.exports= scrapePrice;
